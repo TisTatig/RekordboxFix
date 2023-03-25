060c9b800aea283e8e43d5a11b6a6d7f299ab102
TisTatig <tistatig@gmail.com>
2023-03-25 01:27:00 +0100

    First run at changing the function to return a DataTable for easy usage in main.dart

--- lib/remove_duplicates.dart
+++ lib/remove_duplicates.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:xml/xml.dart';
 import 'dart:io';
 
-List<String> findDuplicates(File file) {
+DataTable findDuplicates(File file) {
   final document = XmlDocument.parse(file.readAsStringSync());
   final trackList = document.findAllElements('TRACK');
-  List<String> duplicateList = [];
+  List<DataRow> duplicateList = [];
 
   trackList.forEach((element) {
     var testName = element.getAttribute('Name');
@@ -21,7 +22,15 @@ List<String> findDuplicates(File file) {
         matchSize == testSize &&
         !(testID == matchID)) {
       // IN PROGRESS: Populating the list of duplicates
-      duplicateList.add("$testName - $testArtist");
+      // The idea is to use List<Datacolumn> duplicateInfo { return [DataRow(Cells:[ Datacell(Text:'$TrackID'), etc, etc, etc]}
+      // then those lists will be put into a DataTable through:
+      //  when duplicate is found: duplicateInfo.add.duplicateInfo()
+
+      // When the list is filled create DataTable with:
+      // DataTable duplicateTable() { return DataTable(columns: Properties(), rows: duplicateList)}
+      // and then return that DataTable
+      
+      duplicateList.addAll("$matchID" : "$testName - $testArtist");
     }
   });
 
