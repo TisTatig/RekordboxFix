83f4e4cb795032edc090ca6b2eb5b601605898a1
TisTatig <tistatig@gmail.com>
2023-03-25 01:51:17 +0100

    Added comments for clarity

--- lib/remove_duplicates.dart
+++ lib/remove_duplicates.dart
@@ -5,42 +5,50 @@ import 'dart:io';
 DataTable findDuplicates(File file) {
   final document = XmlDocument.parse(file.readAsStringSync());
   final trackList = document.findAllElements('TRACK');
+  // Creating empty list to house the info of the duplicates in DataRow form for the DataTable
   List<DataRow> duplicateList = [];
+
+  // Creating the column labels of the DataTable
   List<DataColumn> propertyList = [
     const DataColumn(label: Text('ID')),
     const DataColumn(label: Text('Name')),
     const DataColumn(label: Text('Artist'))
   ];
 
-  trackList.forEach((element) {
-    var testName = element.getAttribute('Name');
-    var testID = element.getAttribute('TrackID');
-    var testArtist = element.getAttribute('Artist');
-    var testSize = element.getAttribute('Size');
-    var match = trackList
-        .lastWhere((element) => element.getAttribute('Name') == testName);
-    var matchID = match.getAttribute('TrackID');
-    var matchArtist = element.getAttribute('Artist');
-    var matchSize = element.getAttribute('Size');
+  //Finding the matches
+  trackList.forEach(
+    (element) {
+      var testName = element.getAttribute('Name');
+      var testID = element.getAttribute('TrackID');
+      var testArtist = element.getAttribute('Artist');
+      var testSize = element.getAttribute('Size');
+      var match = trackList
+          .lastWhere((element) => element.getAttribute('Name') == testName);
+      var matchID = match.getAttribute('TrackID');
+      var matchArtist = element.getAttribute('Artist');
+      var matchSize = element.getAttribute('Size');
 
-    if (matchArtist == testArtist &&
-        matchSize == testSize &&
-        !(testID == matchID)) {
-      // IN PROGRESS: Populating the list of duplicates
-      // The idea is to use
-      List<DataRow> duplicateInfo() {
-        return [
-          DataRow(cells: [
-            DataCell(Text('$matchID')),
-            DataCell(Text('$testName')),
-            DataCell(Text('$matchArtist')),
-          ])
-        ];
+      // If match is found a new DataRow item is created and appended to the duplicateList
+      if (matchArtist == testArtist &&
+          matchSize == testSize &&
+          !(testID == matchID)) {
+        //Creating the DataRow packages of the found duplicate's info to be used in building the DataTable
+        List<DataRow> duplicateInfo() {
+          return [
+            DataRow(
+              cells: [
+                DataCell(Text('$matchID')),
+                DataCell(Text('$testName')),
+                DataCell(Text('$matchArtist')),
+              ],
+            )
+          ];
+        }
+
+        duplicateList.add(duplicateInfo().first);
       }
-
-      duplicateList.add(duplicateInfo().first);
-    }
-  });
+    },
+  );
 
   return DataTable(
     columns: propertyList,
